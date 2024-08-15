4d4d8e7716515ab4970ff5d4864c224d3284c159
ebolo1410 <112871027+ebolo1410@users.noreply.github.com>
2024-08-15 15:37:05 +0700

    File Upload .NET

---
+++ Upload.aspx.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace FileUploadSSRF
+{
+	public partial class Upload : System.Web.UI.Page
+	{
+		protected void Page_Load(object sender, EventArgs e)
+		{
+			if (!IsPostBack)
+			{
+				BindUploadedFiles();
+			}
+		}
+
+		protected void UploadButton_Click(object sender, EventArgs e)
+		{
+			if (FileUploadControl.HasFile)
+			{
+				try
+				{
+					string filename = Path.GetFileName(FileUploadControl.FileName);
+					string uploadPath = Server.MapPath("/uploads/" + filename);
+					FileUploadControl.SaveAs(uploadPath);
+					StatusLabel.Text = "Upload status: File uploaded";
+					BindUploadedFiles();
+				}
+				catch (Exception ex)
+				{
+					StatusLabel.Text = "Upload status: The file could not be uploaded";
+				}
+			}
+		}
+
+		private void BindUploadedFiles()
+		{
+			var files = Directory.GetFiles(Server.MapPath("/uploads/"));
+			var fileList = new System.Collections.Generic.List<object>();
+
+			foreach (var file in files)
+			{
+				fileList.Add(new
+				{
+					FileName = Path.GetFileName(file),
+					FilePath = "/uploads/" + Path.GetFileName(file),
+				});
+			}
+			UploadedFileRepeater.DataSource = fileList;
+			UploadedFileRepeater.DataBind();
+		}
+
+		
+
+	}
+}
